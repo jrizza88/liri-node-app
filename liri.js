c1950de85561d369e948a616678b4bc723487879
jrizza88 <jetorres88@gmail.com>
2016-07-13 15:48:43 -0400

    updates

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 // Listing required variables to make the application work 
 
-var twitterKeys = require('./keys');
+var twitterKeys = require('./keys').twitterKeys;
 
 var Twitter = require('twitter');
 
@@ -8,40 +8,52 @@ var spotify = require('spotify');
 
 var request = require('request');
 
+var twitter = new Twitter(twitterKeys);
+
 // variables to call the process in node
 
-var tweets = process.argv.[2];
-var parameter = process.argv[3];
+var userRequests = process.argv[2];
+var param = process.argv[3];
 
 // function that calls user's request
 
-userRequestsFunc(userRequests, paramaters);
+userRequestsFunc(userRequests, param);
 
 // calling the user's requests
 
-function userRequestsFunc(userRequests, parameters){
+function userRequestsFunc(userRequests, param){
+// using switch and case to call the individual functions
+          switch (userRequests) {
 
-  request.prompt([
+          case "my-tweets" :
+                            twitter();
+                            break;
 
-  // my-tweets
+          case "spotify-this-song" :
+                            spotifyRequest();
+                            break;
 
-  // spotify-this-song
+          case "movie-this" :
+                            getMovies();
+                            break;
 
-  // movie-this
-
-  // do-what-it-says
-])
+          case "do-what-it-says" :
+                            doWhatItSays();
+                            break;
+          default: 
+  console.log("Choices displayed: \nmy-tweets\nspotify-this-song\nmovie-this\ndo-what-it-says\n");
+        }
 }
 
 
-tweets.get(path, params, callback);
-tweets.post(path, params, callback);
+//tweets.get(path, params, callback);
+//tweets.post(path, params, callback);
 
 // checks the user's requests for Twitter
 
 // using callback function to call the twitter tweets 
 
-
+function twitter() {
 
 var params = {screen_name: '@jrizza88'};
 params.get('statuses/user_timeline', params, function(error, tweets, response){
@@ -50,14 +62,10 @@ params.get('statuses/user_timeline', params, function(error, tweets, response){
   }
 });
 
-
-client.post('statuses/update', {status: 'I Love Twitter'},  function(error, tweet, response){
-  if(error) throw error;
-  console.log(tweet);  // Tweet body. 
-  console.log(response);  // Raw response object. 
-});
-
 // checks user's requests for Spotify
+
+function spotifyRequest(music) {
+
  
 spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
     if ( err ) {
@@ -67,5 +75,16 @@ spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(er
 
     // Do something with 'data' 
 });
+}
+// checks user's requests for movies 
 
+function getMovies (movies) {
+
+}
+
+// do what is says function
+
+function doWhatItSays() {
+
+}
 
